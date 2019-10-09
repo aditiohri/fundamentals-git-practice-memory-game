c5e6e291c44f973286b6364150edd1e786eb1b2c
Aditi Ohri <didiohri@gmail.com>
2019-10-08 21:02:32 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,40 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	image: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	image: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	image: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	image: "images/king-of-diamonds.png"
+},
+];
 
 var cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) { 
 	alert("You found a match!");
 	} else { alert("Sorry, try again."); }
-	} 
-}
+	}; 
+
 
 function flipCard(cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].image);
+console.log(cards[cardId].suit);
 checkForMatch();
 }
 
